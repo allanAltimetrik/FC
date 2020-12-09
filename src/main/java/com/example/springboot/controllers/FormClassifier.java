1c67eb7ed62d5d348a9187162ab8588a4e68dbd3
QueerPassenger <allanmvalooran@gmail.com>
2020-12-09 22:01:03 +0530

    FineTunedSwaggerConfig

--- src/main/java/com/example/springboot/controllers/FormClassifier.java
+++ src/main/java/com/example/springboot/controllers/FormClassifier.java
@@ -2,12 +2,13 @@ package com.example.springboot.controllers;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "Form classifier", description = "Api Endpoints for Form Classifier")
 public class FormClassifier {
-    @RequestMapping("/modal")
+    @RequestMapping(value="/modal", method = RequestMethod.GET)
     public String modalFunction(){
         return "Lets Start";
     }
