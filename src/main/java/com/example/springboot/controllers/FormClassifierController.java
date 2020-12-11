9780fb650e693427fd8968f9ac2462ec75b6c5f2
allanAltimetrik <avalooran@altimetrik.com>
2020-12-11 19:56:48 +0530

    API-EndpointsForProcessInputAndSample

--- src/main/java/com/example/springboot/controllers/FormClassifierController.java
+++ src/main/java/com/example/springboot/controllers/FormClassifierController.java
@@ -5,7 +5,9 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Api(tags = "Form classifier", description = "Api Endpoints for Form Classifier")
@@ -19,5 +21,13 @@ public class FormClassifierController {
     @RequestMapping(value="/modal", method = RequestMethod.GET)
     public String modalFunction(){
         return formClassifierService.getModalString();
+    }
+    @RequestMapping(value="/processInputFile", method = RequestMethod.POST)
+    public String processInputFile(@RequestParam("file") MultipartFile file){
+        return "File Name " + file.getOriginalFilename();
+    }
+    @RequestMapping(value="/processSampleFile", method = RequestMethod.POST)
+    public String processSampleFile(@RequestParam("file") MultipartFile file, @RequestParam("type") String type){
+        return "File Name " + file.getOriginalFilename() + " is a sample of " + type + " form";
     }
 }
