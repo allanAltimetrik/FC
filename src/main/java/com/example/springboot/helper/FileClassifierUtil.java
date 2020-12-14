40c7509db03ec53ae1b602cf7647efe76610e3bf
Ramesh Krishnan <rameshkrishnan.ganesan@gmail.com>
2020-12-14 13:05:33 +0530

    Added Util for File Classification

---
+++ src/main/java/com/example/springboot/helper/FileClassifierUtil.java
@@ -0,0 +1,99 @@
+package com.example.springboot.helper;
+
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.configuration.ConfigurationException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class FileClassifierUtil {
+
+	public static void main(String[] args) throws TesseractException, IOException, ConfigurationException {
+		//classifyForms("src/main/java/com/example/springboot/resources/samples/Kansas Birth Certificate.jpg");
+		
+		
+		
+		
+		
+	}
+
+	public static String classifyForms(String file) throws TesseractException, IOException {
+
+		// Read All Keywords from Keywords File
+		Hashtable<String, List<String>> keywords = PropertyFileUtil.readAllKeywords();
+
+		// Extracting Text from Image
+		String extractedText = OcrUtil.extractTextFromImage(file);
+
+		// Process Extracted Text
+		String processedText = ExtractTextUtil.processExtractedText(extractedText);
+
+		// Convert the Processed Text to List
+		String[] processedTextArray = processedText.split("\\s+");
+		List<String> processedTextList = Arrays.asList(processedTextArray);
+		System.out.println("Processed Text List - " + processedTextList);
+
+		// Comparing the Processed Text with Keywords
+		Hashtable<String, Integer> matchingKeyWords = new Hashtable<>();
+		Set<Map.Entry<String, List<String>>> entries = keywords.entrySet();
+		Iterator<Map.Entry<String, List<String>>> itr = entries.iterator();
+		while (itr.hasNext()) {
+			Map.Entry<String, List<String>> entry = itr.next();
+			String key = entry.getKey();
+			List<String> value = new ArrayList<String>(entry.getValue());
+			System.out.println(value);
+			List<String> extractedTextList = new ArrayList<String>(processedTextList);
+			System.out.println(extractedTextList);
+			extractedTextList.retainAll(value);
+			matchingKeyWords.put(key, extractedTextList.size());
+		}
+		System.out.println("File Name - Number of Keywords Matched : " + matchingKeyWords);
+
+		// Finding Greater Number of Matching Keyword
+		List<Integer> numberOfMatchingWords = new ArrayList<Integer>();
+		Set<Entry<String, Integer>> matchingText = matchingKeyWords.entrySet();
+		Iterator<Entry<String, Integer>> itr1 = matchingText.iterator();
+		while (itr1.hasNext()) {
+			Entry<String, Integer> entry = itr1.next();
+			int value = entry.getValue();
+			numberOfMatchingWords.add(value);
+		}
+		Collections.sort(numberOfMatchingWords, Collections.reverseOrder());
+		int greatestValue = numberOfMatchingWords.get(0);
+		System.out.println("GreatestValue - " + greatestValue);
+
+		// Get the File Type for Value
+		String fileType = null;
+		if (greatestValue > 0) {
+			fileType = getKeyFromValue(matchingKeyWords, greatestValue).toString();
+		} else {
+			fileType = "unknown file";
+		}
+		System.out.println("File Type - " + fileType);
+		return fileType;
+	}
+
+	public static Object getKeyFromValue(Map<String, Integer> hm, Object value) {
+		for (Object o : hm.keySet()) {
+			if (hm.get(o).equals(value)) {
+				return o;
+			}
+		}
+		return null;
+	}
+	
+	public static void iterateOverFiles(String directory) {		
+		File folder = new File(directory);
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+		  if (listOfFiles[i].isFile()) {
+		    System.out.println("File " + listOfFiles[i].getName());
+		  } else if (listOfFiles[i].isDirectory()) {
+		    System.out.println("Directory " + listOfFiles[i].getName());
+		  }
+		}
+	}
+
+}
