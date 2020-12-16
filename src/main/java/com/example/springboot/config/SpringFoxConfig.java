b0fb69b0882c9349e9792c95e2d23d8ebee45926
Ramesh Krishnan <rameshkrishnan.ganesan@gmail.com>
2020-12-16 13:30:34 +0530

    Updated Properties.Util

--- src/main/java/com/example/springboot/config/SpringFoxConfig.java
+++ src/main/java/com/example/springboot/config/SpringFoxConfig.java
@@ -1,9 +1,7 @@
 package com.example.springboot.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
--- src/main/java/com/example/springboot/helper/test.java
+++ src/main/java/com/example/springboot/helper/test.java
@@ -1,11 +1,11 @@
 package com.example.springboot.helper;
 
-import java.io.File;
 
 public class test {
     public static void main(String[] args) {
         try {
-          
+        	
+        	
         }
         catch (Exception ex){
 
